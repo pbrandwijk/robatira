80246cc62528768bd58b1a15407aa0ec9076447f
Pieter Brandwijk <pieter@pbrandwijk.com>
2017-04-06 09:09:49 +1200

    Add basic data definition of playing cards

--- src/Robatira/Model/Cards.hs
+++ src/Robatira/Model/Cards.hs
@@ -1,3 +1,23 @@
-module Robatira.Model.Cards where
+module Robatira.Model.Cards 
 
+where
+
+
+data Suit = Spades | Hearts | Diamonds | Clubs deriving (Show, Eq, Enum)
+
+data Rank = Ace | Two | Three | Four | Five | Six | Seven | Eight | Nine | 
+              Ten | Jack | Queen | King deriving (Show, Eq, Enum)
+
+data Joker = RedJoker | BlackJoker | WhiteJoker deriving (Show, Eq, Enum)
+
+data Card = Regular Suit Rank | Other Joker deriving (Show, Eq)
+
+
+allSuits = [Hearts ..]
+allRanks = [Two ..]
+allJokers = [RedJoker ..]
+
+frenchStandard52Plus3Jokers :: [Card]
+frenchStandard52Plus3Jokers = [Regular c f | c <- allSuits, f <- allRanks] 
+            ++ [Other j | j <- allJokers]
 
