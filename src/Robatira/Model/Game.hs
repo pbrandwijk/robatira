9e200a4c5749031e3ac2fabd24fc6137ce69f512
Pieter Brandwijk <pieter@pbrandwijk.com>
2017-04-06 09:46:43 +1200

    Add starting of game function and stub for game play

--- src/Robatira/Model/Game.hs
+++ src/Robatira/Model/Game.hs
@@ -3,6 +3,7 @@ module Robatira.Model.Game
 where
 
 import Robatira.Model.Cards
+import Robatira.Util.Shuffle (shuffle)
 
 
 data PlayerType = Human | Computer deriving (Show)
@@ -28,3 +29,21 @@ initGame startDeck = Game startStack throwingStack player1 [player2]
    startStack = deckAfterthrowingStack
    player1 = Player Human "Alice" hand1
    player2 = Player Human "Bob" hand2
+
+-- Starting the game is setting up the initial game state and starting off the 
+-- first player
+start :: IO ()
+start = do
+  shuffledDeck <- shuffle frenchStandard52Plus3Jokers
+  let initializedGame = initGame shuffledDeck
+  play initializedGame
+  
+
+-- Playing the game means the current player either picks a card from the dealing
+-- stack or throws a legal card on the throwing stack. 
+-- If the current player after this has no more cards left, the game is over.
+-- Else, the next player on the list becomes the current player and the whole process
+-- reiterates
+play :: Game -> IO ()
+play game = do
+  putStrLn $ show game
