219cc53ab6cbc7cad279449e3becfa6e02fbe183
Pieter Brandwijk <pieter@pbrandwijk.com>
2017-11-18 13:04:44 +0100

    Add initial module for GameInfo

---
+++ src/Robatira/Model/GameInfo.hs
@@ -0,0 +1,7 @@
+module Robatira.Model.GameInfo
+
+where
+
+-- This module defines the data that is available to each player in order to
+-- play the game. This data can be used for visualization to human players
+-- and AI for the computer players.
